074adb65e3b719032db483c8e56350d61709afa0
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-04-18 12:23:12 +0200

    Simplify app

--- BeefyAlgorithm.py
+++ BeefyAlgorithm.py
@@ -8,6 +8,6 @@ import streamlit as st
 
 def beefy_algorithm(date: date) -> pd.DataFrame:
     time.sleep(5)
-    dates = pd.date_range(date, periods=10)
-    values = np.arange(10)
+    dates = pd.date_range(date, periods=3)
+    values = np.arange(3)
     return pd.DataFrame({"date": dates, "value": values})
