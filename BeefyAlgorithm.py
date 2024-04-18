2cc0dda905c94491faadf581fcdee23269b1bacb
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-04-18 12:10:50 +0200

    Beefy app

--- BeefyAlgorithm.py
+++ BeefyAlgorithm.py
@@ -6,7 +6,6 @@ import numpy as np
 
 import streamlit as st
 
-@st.cache_data(show_spinner=False)
 def beefy_algorithm(date: date) -> pd.DataFrame:
     time.sleep(5)
     dates = pd.date_range(date, periods=10)
--- beefy_app.py
+++ beefy_app.py
@@ -1,15 +1,9 @@
 import streamlit as st
-from datetime import date
-
-from BeefyAlgorithm import beefy_algorithm
 
 st.title('Beefy Algorithm App')
 
-a_date: date = st.date_input("Date for Beefy Algorithm")
+some_date = st.date_input("Some date:")
 
-btn_pressed: bool = st.button('Run Beefy Algorithm')
+btn_pressed: bool = st.button('Run Algorithm')
 if btn_pressed:
-    with st.spinner("Computing..."):
-        df = beefy_algorithm(a_date)
-    st.subheader('Results:')
-    st.write(df.head())
+    st.write("Date selected: ", some_date)
